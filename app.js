3042db9f5d592118e4372d91e1a2a03217dc678e
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-01-23 18:21:10 -0600

    Save action log in turns variable

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function () {
@@ -13,7 +14,13 @@ new Vue({
     },
 
     attack: function () {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      let damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster for ${damage}`
+      })
+
       if (this.checkWin()) {
         return;
       }
@@ -45,8 +52,14 @@ new Vue({
     },
 
     monsterAttacks: function () {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      let damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
       this.checkWin();
+
+      this.turns.unshift({
+        isPlayer: false,
+        text: `Monster hits Player for ${damage}`
+      })
     },
 
     calculateDamage: function (min, max) {
