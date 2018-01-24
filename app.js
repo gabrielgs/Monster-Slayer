75148431540a3f9c600696debad749c13e308094
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-01-23 18:41:30 -0600

    Finish log

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.monsterHealth = 100;
       this.playerHealth = 100;
+      this.turns = [];
     },
 
     attack: function () {
@@ -29,7 +30,14 @@ new Vue({
     },
 
     specialAttack: function (){
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      let damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster hard for ${damage}`
+      })
+
       if (this.checkWin()) {
         return;
       }
@@ -44,6 +52,11 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heal for 10'
+      })
       this.monsterAttacks();
     },
 
